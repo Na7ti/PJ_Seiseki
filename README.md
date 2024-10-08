1032312e00ab64cfb92f8c73cfb74614a8ac4ca4
Na7ti <mamak06mamak13@gmail.com>
2024-10-08 10:44:08 +0900

    remeke commit

---
+++ README.md
@@ -0,0 +1,2 @@
+> 引用元:
+> https://qiita.com/PoKoPoKoTa2ry/items/2f9b54518b8158809192
---
+++ vue_project/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+const app = createApp(App)
+
+app.config.productionTip = false
+app.use(VueAxios, axios)
+
+app.mount('#app')
